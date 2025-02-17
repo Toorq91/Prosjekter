34839f9c6e2e0eca2c5891ac6ae03cc6c0c18488
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-02-17 14:53:20 +0100

    x

--- Meg/src/views/Interests.vue
+++ Meg/src/views/Interests.vue
@@ -2,10 +2,13 @@
   <div class="flex flex-col items-center justify-center min-h-screen bg-gray-600 text-center">
     <h1 class="text-2xl font-bold">Mine interesser</h1>
     <ul class="mt-4 list-disc pl-6">
-      <li>🎮 Gaming</li>
-      <li>📖 Lese bøker</li>
-      <li>🎶 Musikk</li>
-      <li>💻 Programmering</li>
+      <ul>
+        <li>🥏 Disc golf</li>
+        <li>📺 TV series/Movies/Anime</li>
+        <li>🎮 Gaming</li>
+        <li>🎶 Musikk</li>
+        <li>💻 Programmering</li>
+      </ul>
     </ul>
   </div>
 </template>
