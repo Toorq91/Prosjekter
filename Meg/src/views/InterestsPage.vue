92e2e07d3c3ffc3aeb5783ce8e2e2450e72442a1
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-17 16:51:37 +0100

    ny style

--- Meg/src/views/InterestsPage.vue
+++ Meg/src/views/InterestsPage.vue
@@ -3,12 +3,12 @@
     <h1 class="text-6xl font-bold text-red-500">Mine interesser</h1>
     <ul class="text-2xl mt-4 list-disc border-6 border-red-500">
       <ul>
-        <li class="border-2 border-red-500 text-4xl text-green-600">
+        <li class="px-4 py-2 bg-gradient-to-tr from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
           <RouterLink to="/discgolf" class="block px-4 py-2 hover:text-red-400 transition">
           🥏 Disc golf
         </RouterLink>
       </li>
-        <li class="border-2 border-red-500 text-4xl text-green-600">
+        <li class="px-4 py-2 bg-gradient-to-bl from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
           <RouterLink to="/tv" class="block px-4 py-2 hover:text-red-400 transition">
           📺 TV series/Movies/Anime
         </RouterLink>
--- Meg/src/views/homePage.vue
+++ Meg/src/views/homePage.vue
@@ -2,8 +2,8 @@
   <div class="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-center">
     <h1 class="text-3xl font-bold mb-6 text-red-500">Velkommen til siden om meg</h1>
     <div class="space-x-4">
-      <RouterLink to="/about" class="px-4 py-2 bg-red-500 text-neutral-900 rounded text-4xl font-semibold">Om meg</RouterLink>
-      <RouterLink to="/interests" class="px-4 py-2  bg-neutral-900 text-red-500 rounded text-4xl font-semibold border border-red-500">Interesser</RouterLink>
+      <RouterLink to="/about" class="px-4 py-2 bg-gradient-to-r from-red-500 to-yellow-500 text-neutral-900 rounded text-4xl font-semibold">Om meg</RouterLink>
+      <RouterLink to="/interests" class="px-4 py-2 bg-gradient-to-l from-red-500 to-yellow-500 text-netural-900 rounded text-4xl font-semibold border border-red-500">Interesser</RouterLink>
     </div>
   </div>
 </template>
