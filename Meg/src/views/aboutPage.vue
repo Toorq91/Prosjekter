7932381d3fe48e188454cb59f95f82238f07186d
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-18 14:44:52 +0100

    kommet igang med aboutPage

--- Meg/src/views/aboutPage.vue
+++ Meg/src/views/aboutPage.vue
@@ -1,30 +1,17 @@
-<!-- <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-center">
-    <h1 class="underline text-6xl font-bold text-red-500 ">Om meg:</h1>
-    <p class="mt-2 text-white font-bold">Test asefopuhsregousrg</p>
-  </div>
-</template> -->
-
-
 <template>
   <div class="bg-neutral-900 min-h-screen">
     <h1 class="flex flex-col items-center text-center text-6xl font-bold underline w-full py-20
-  text-transparent bg-clip-text bg-gradient-to-t from-yellow-500  to-red-500 shadow-xl shadow-red-500">
-  Thor Christer Halvorsen Eikeland:
-</h1>
-    <div class="flex flex-col items-center justify-center min-h-screen text-center -mt-60">
-    <div class="p-[10px] bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70 rounded-lg">
-      <p>
-        saeofijsrgoåisrjgseroguhsrhgosrg
-      </p>
-      <p>
-        srgopusrhgopusrhgnsrg
-      </p>
-      <p>
-        srgoushrgousrhg
-      </p>
+      text-transparent bg-clip-text bg-gradient-to-t from-yellow-500 to-red-500 shadow-xl shadow-red-500">
+      Thor Christer Halvorsen Eikeland:
+    </h1>
+    <div class="flex flex-col items-center justify-center min-h-screen text-center -mt-100">
+      <div class="p-6 rounded-lg bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70">
+        <div class="p-6 rounded-lg bg-neutral-900 text-white text-xl space-y-2">
+          <p>saeofijsrgoåisrjgseroguhsrhgosrg aoeihjhsreoghsregsergpåisjegoisrejaseopguhsegopusaheg</p>
+          <p>srgopusrhgopusrhgn asegpåoijsrgopjhsroåpgijhsreh srgoisjrgopiusrgjhsrg</p>
+          <p>srgoushrgousraefopusheg seioguhse9guhsr9iughsrg pioushegiopushrghg</p>
+        </div>
+      </div>
     </div>
   </div>
-</div>
 </template>
-
