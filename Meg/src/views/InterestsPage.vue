9ce8df21c964d5caa21c33898a781e9cf6a1da90
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-18 13:07:48 +0100

    fikset det meste på interestspage

--- Meg/src/views/InterestsPage.vue
+++ Meg/src/views/InterestsPage.vue
@@ -1,34 +1,46 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-center">
-    <h1 class="text-6xl font-bold text-red-500">Mine interesser</h1>
-    <ul class="text-2xl mt-4 list-disc border-6 border-green-500">
+  <div class="bg-neutral-900 min-h-screen">
+    <h1 class="flex flex-col items center text-center text-6xl font-bold text-red-500 underline w-full py-30">Mine interesser:</h1>
+    <div class="flex flex-col items-center justify-center min-h-screen text-center -mt-80">
+    <ul class="p-[10px] bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70 rounded-lg">
       <ul>
-        <li class="px-4 py-2 bg-gradient-to-b from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
-         <RouterLink to="/discgolf" class="block px-4 py-2 hover:text-blue-400 transition">
-          🥏 Disc golf
-         </RouterLink>
-      </li>
-        <li class="px-4 py-2 bg-gradient-to-b from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
-         <RouterLink to="/tv" class="block px-4 py-2 hover:text-blue-400 transition">
-          📺 TV series/Movies/Anime
-         </RouterLink>
-      </li>
-        <li class="px-4 py-2 bg-gradient-to-b from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
-          <RouterLink to="/gaming" class="block px-4 py-2 hover:text-blue-400 transition">
-          🎮 Gaming
+        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        bg-gradient-to-b from-red-500 to-yellow-500
+        hover:bg-gradient-to-t hover:from-yellow-500 hover:to-red-500">
+        <RouterLink to="/discgolf" class="block px-4 py-2 hover:text-blue-500/70 transition duration-300">
+            🥏 Disc golf
           </RouterLink>
         </li>
-        <li class="px-4 py-2 bg-gradient-to-b from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
-          <RouterLink to="/music" class="block px-4 py-2 hover:text-blue-400 transition">
-          🎶 Musikk
+        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        bg-gradient-to-b from-red-500 to-yellow-500
+        hover:bg-gradient-to-t hover:from-yellow-500 hover:to-red-500">
+          <RouterLink to="/tv" class="block px-4 py-2 hover:text-blue-500/70 transition duration-300">
+            📺 TV series/Movies/Anime
           </RouterLink>
         </li>
-        <li class="px-4 py-2 bg-gradient-to-b from-red-500 to-yellow-500 text-neutral-900 text-4xl font-semibold">
-          <RouterLink to="/programmering" class="block px-4 py-2 hover:text-blue-400 transition">
-          💻 Programmering
+        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-t hover:from-yellow-500 hover:to-red-500">
+          <RouterLink to="/gaming" class="block px-4 py-2 hover:text-blue-500/70 transition duration-300">
+            🎮 Gaming
+          </RouterLink>
+        </li>
+        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-t hover:from-yellow-500 hover:to-red-500">
+          <RouterLink to="/music" class="block px-4 py-2 hover:text-blue-500/70 transition duration-300">
+            🎶 Musikk
+          </RouterLink>
+        </li>
+        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-t hover:from-yellow-500 hover:to-red-500">
+          <RouterLink to="/programmering" class="block px-4 py-2 hover:text-blue-500/70 transition duration-300">
+            💻 Programmering
           </RouterLink>
         </li>
       </ul>
     </ul>
   </div>
+</div>
 </template>
