e6ee60dddf23306f1dde57417d77a2c0f33cac78
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-18 14:35:14 +0100

    begynt på aboutPage

--- Meg/src/views/InterestsPage.vue
+++ Meg/src/views/InterestsPage.vue
@@ -1,41 +1,41 @@
 <template>
   <div class="bg-neutral-900 min-h-screen">
     <h1 class="flex flex-col items-center text-center text-6xl font-bold underline w-full py-20
-  text-transparent bg-clip-text bg-gradient-to-l from-yellow-500  to-red-500 shadow-xl shadow-red-500">
+  text-transparent bg-clip-text bg-gradient-to-t from-yellow-500  to-red-500 shadow-xl shadow-red-500">
   Mine interesser:
 </h1>
     <div class="flex flex-col items-center justify-center min-h-screen text-center -mt-80">
     <ul class="p-[10px] bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70 rounded-lg">
       <ul>
-        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        <li class="w-[700px] py-2 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
         bg-gradient-to-b from-red-500 to-yellow-500
         hover:bg-gradient-to-b hover:from-yellow-500/40 hover:to-red-500/40">
         <RouterLink to="/discgolf" class="block px-4 py-2 hover:text-blue-500/70 transition duration-500">
             🥏 Disc golf
           </RouterLink>
         </li>
-        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        <li class="w-[700px] py-2 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
         bg-gradient-to-b from-red-500 to-yellow-500
         hover:bg-gradient-to-b hover:from-yellow-500/40 hover:to-red-500/40">
           <RouterLink to="/tv" class="block px-4 py-2 hover:text-blue-500/70 transition duration-500">
             📺 TV series/Movies/Anime
           </RouterLink>
         </li>
-        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        <li class="w-[700px] py-2 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
                    bg-gradient-to-b from-red-500 to-yellow-500
                    hover:bg-gradient-to-b hover:from-yellow-500/40 hover:to-red-500/40">
           <RouterLink to="/gaming" class="block px-4 py-2 hover:text-blue-500/70 transition duration-500">
             🎮 Gaming
           </RouterLink>
         </li>
-        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        <li class="w-[700px] py-2 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
                    bg-gradient-to-b from-red-500 to-yellow-500
                    hover:bg-gradient-to-b hover:from-yellow-500/40 hover:to-red-500/40">
           <RouterLink to="/music" class="block px-4 py-2 hover:text-blue-500/70 transition duration-500">
             🎶 Musikk
           </RouterLink>
         </li>
-        <li class="px-4 py-2 text-neutral-900 text-4xl font-semibold transition duration-500 ease-in-out
+        <li class="w-[700px] py-2 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
                    bg-gradient-to-b from-red-500 to-yellow-500
                    hover:bg-gradient-to-b hover:from-yellow-500/40 hover:to-red-500/40">
           <RouterLink to="/programmering" class="block px-4 py-2 hover:text-blue-500/70 transition duration-500">
--- Meg/src/views/homePage.vue
+++ Meg/src/views/homePage.vue
@@ -1,9 +1,50 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen bg-neutral-900 text-center">
-    <h1 class="text-3xl font-bold mb-6 text-red-500">Velkommen til siden om meg</h1>
-    <div class="space-x-4">
-      <RouterLink to="/about" class="px-4 py-2 bg-gradient-to-r from-red-500 to-yellow-500 text-neutral-900 rounded text-4xl font-semibold">Om meg</RouterLink>
-      <RouterLink to="/interests" class="px-4 py-2 bg-gradient-to-l from-red-500 to-yellow-500 text-netural-900 rounded text-4xl font-semibold border border-red-500">Interesser</RouterLink>
-    </div>
+  <div class="bg-neutral-900 min-h-screen">
+    <h1 class="flex flex-col items-center text-center text-6xl font-bold underline w-full py-20
+  text-transparent bg-clip-text bg-gradient-to-t from-yellow-500  to-red-500 shadow-xl shadow-red-500">
+  Hjem:
+</h1>
+    <div class="flex flex-col items-center justify-center min-h-screen text-center -mt-60">
+    <ul class="p-[10px] bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70 rounded-lg">
+      <ul>
+        <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+        bg-gradient-to-b from-red-500 to-yellow-500
+        hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
+        <RouterLink to="/about" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
+             Om meg
+          </RouterLink>
+        </li>
+        <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+        bg-gradient-to-b from-red-500 to-yellow-500
+        hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
+          <RouterLink to="/interests" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
+            Interesser
+          </RouterLink>
+        </li>
+        <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
+          <RouterLink to="/" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
+            Loading...
+          </RouterLink>
+        </li>
+        <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
+          <RouterLink to="/" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
+            Loading...
+          </RouterLink>
+        </li>
+        <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+                   bg-gradient-to-b from-red-500 to-yellow-500
+                   hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
+          <RouterLink to="/" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
+            Loading...
+          </RouterLink>
+        </li>
+      </ul>
+    </ul>
   </div>
+</div>
 </template>
+
