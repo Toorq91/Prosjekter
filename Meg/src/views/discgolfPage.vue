019c10304ef818ebf4956a217bd5a3cde030c145
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-20 14:22:02 +0100

    fikset bakgrunn for interesser sidene

--- Meg/src/views/discgolfPage.vue
+++ Meg/src/views/discgolfPage.vue
@@ -1,15 +1,20 @@
 <template>
-  <div class="bg-neutral-900 min-h-screen">
-    <h1 class="flex flex-col items-center text-center text-6xl font-bold underline w-full py-10
-      text-transparent bg-clip-text bg-gradient-to-t from-yellow-500 to-red-500 shadow-xl shadow-red-500">
-      Disc golf:
-    </h1>
-    <div class="flex flex-col items-center justify-center flex-1 text-center py-30">
-      <div class="p-[20px] rounded-lg bg-gradient-to-r from-red-500/60 via-yellow-500/70 to-yellow-500/20">
-        <div class="p-[20px] rounded-lg bg-neutral-900 text-white text-xl space-y-2">
-          <p>saeofijsrgoåisrjgseroguhsrhgosrg aoeihjhsreoghsregsergpåisjegoisrejaseopguhsegopusaheg</p>
-          <p>srgopusrhgopusrhgn asegpåoijsrgopjhsroåpgijhsreh srgoisjrgopiusrgjhsrg</p>
-          <p>srgoushrgousraefopusheg seioguhse9guhsr9iughsrg pioushegiopushrghg</p>
+  <div class="relative min-h-screen bg-neutral-900">
+    <div class="absolute inset-0 bg-[url('/img/discgolf.jpg')] bg-cover bg-center">
+      <div class="absolute inset-0 bg-white opacity-20"></div>
+    </div>
+    <div class="relative z-10 flex flex-col items-center text-center min-h-screen">
+      <h1 class="text-6xl font-bold py-10 text-transparent bg-clip-text
+        bg-gradient-to-t from-yellow-500 to-red-500">
+        Disc golf:
+      </h1>
+      <div class="flex flex-col justify-center flex-1">
+        <div class="p-[20px] rounded-lg border-2 border-yellow-500">
+          <div class="rounded-lg bg-transparent text-white text-xl space-y-2">
+            <p>saeofijsrgoåisrjgseroguhsrhgosrg aoeihjhsreoghsregsergpåisjegoisrejaseopguhsegopusaheg</p>
+            <p>srgopusrhgopusrhgn asegpåoijsrgopjhsroåpgijhsreh srgoisjrgopiusrgjhsrg</p>
+            <p>srgoushrgousraefopusheg seioguhse9guhsr9iughsrg pioushegiopushrghg</p>
+          </div>
         </div>
       </div>
     </div>
