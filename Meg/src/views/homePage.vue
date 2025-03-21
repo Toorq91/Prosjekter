cd7aa8c7bf0a64af04135b855206a3bf69394789
Toorq91 <157486467+Toorq91@users.noreply.github.com>
2025-03-21 10:28:33 +0100

    endret knapp på homePage

--- Meg/src/views/homePage.vue
+++ Meg/src/views/homePage.vue
@@ -8,11 +8,19 @@
     <div class="flex flex-col items-center justify-center flex-1 text-center py-30">
       <ul class="p-[20px] bg-gradient-to-r from-neutral-800/60 via-red-500/60 to-yellow-500/70 rounded-lg">
         <ul>
-          <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
+          <!-- <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
           bg-gradient-to-b from-red-500 to-yellow-500
           hover:bg-gradient-to-b hover:from-yellow-500/60 hover:to-red-500/60">
             <RouterLink to="/about" class="block px-4 py-4 hover:text-blue-500/70 transition duration-500">
               Om meg
+            </RouterLink>
+          </li> -->
+          <li class="w-[700px] py-4 text-5xl font-semibold transition duration-500 ease-in-out
+          flex items-center justify-center
+          bg-gradient-to-b from-red-500 to-yellow-500">
+            <RouterLink to="/about" class="btn-1 block w-full">
+              <span>Om meg</span>
+              <span>Info</span>
             </RouterLink>
           </li>
           <li class="w-[700px] py-4 text-neutral-900 text-5xl font-semibold transition duration-500 ease-in-out
@@ -57,6 +65,42 @@ const images = [
 </script>
 
 <style scoped>
+
+.btn-1 {
+  height: 80px;
+}
+
+.btn-1 span {
+  /* background: rgb(0, 172, 238);
+  background: linear-gradient(0deg, rgba(0, 172, 238, 1) 0%, rgba(2, 126, 251, 1) 100%); */
+  display: block;
+  position: absolute;
+  width: 700px;
+  line-height: 80px;
+  border-radius: 5px;
+  transition: all 0.3s ease-in-out;
+}
+
+.btn-1 span:nth-child(1) {
+  transform: rotateX(90deg);
+  transform-origin: 50% 50% -20px;
+}
+
+.btn-1 span:nth-child(2) {
+  transform: rotateX(0deg);
+  transform-origin: 50% 50% -20px;
+}
+
+.btn-1:hover span:nth-child(1) {
+  transform: rotateX(0deg);
+}
+
+.btn-1:hover span:nth-child(2) {
+  transform: rotateX(-90deg);
+}
+
+
+/* Reklame rullegardin */
 .scrolling-container {
   overflow: hidden;
   white-space: nowrap;
